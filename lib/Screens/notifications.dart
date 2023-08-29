5840b54f0183deb863885d7ab882b20153788c75
Mayuri Nadeesha <nadeeshamayuri15@gmail.com>
2023-08-30 00:02:04 +0530

    created Notification screen

--- lib/Screens/notifications.dart
+++ lib/Screens/notifications.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:mini_project_mobile_app/Components/nav_bar.dart';
 
-class Notifications extends StatelessWidget {
-  const Notifications({super.key});
+class Notifications extends StatefulWidget {
+  const Notifications({Key? key}) : super(key: key);
 
   @override
+  _NotificationsState createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF305D62),
+        title: Text("Notifications"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            NotificationBox(),
+            NotificationBox(),
+            NotificationBox(),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class NotificationBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: MediaQuery.of(context).size.height * 0.2,
+      decoration: BoxDecoration(
+        color: Color(0xFFDFEEEB),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 10), // Added padding
+        child: Text(
+          "Notification",
+          style: TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Notifications(),
+  ));
 }
