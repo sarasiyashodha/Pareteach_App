c0070720fcd97ba4a7468185441b644564183f74
Mayuri Nadeesha <nadeeshamayuri15@gmail.com>
2023-09-05 19:41:39 +0530

    Edited progress, reccomendation and attendance pages

---
+++ lib/Screens/TeachersAccount/setting pages/setting_page1.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mini_project_mobile_app/Screens/TeachersAccount/nav_bar2.dart';
+
+class SettingPage1 extends StatelessWidget {
+  const SettingPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar2(),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF305D62),
+        title: Text("Settings"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage("Images/TeachersProfile.jpeg"),
+            ),
+          ),
+        ],
+      ),
+      
+      
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home",),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search",),
+          BottomNavigationBarItem(icon: Icon(Icons.book_outlined), label: "Academics",),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notifications",),
+        ],
+      ),
+    );
+  }
+}
