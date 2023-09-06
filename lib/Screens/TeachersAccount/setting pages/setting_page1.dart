ec29911b3b64d379664da59ceafdf25c2b1092aa
Mayuri Nadeesha <nadeeshamayuri15@gmail.com>
2023-09-06 14:15:14 +0530

    Edited setting page

--- lib/Screens/TeachersAccount/setting pages/setting_page1.dart
+++ lib/Screens/TeachersAccount/setting pages/setting_page1.dart
@@ -20,18 +20,53 @@ class SettingPage1 extends StatelessWidget {
           ),
         ],
       ),
-      
-      
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Privacy and Security",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Image.asset(
+                  "Images/Privacy&Security.png",
+                  width: 40, // Adjust the width as needed
+                  height: 40, // Adjust the height as needed
+                ),
+              ),
+              Text(
+                "Privacy and Security",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         showSelectedLabels: true,
         showUnselectedLabels: true,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home",),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search",),
-          BottomNavigationBarItem(icon: Icon(Icons.book_outlined), label: "Academics",),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notifications",),
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.book_outlined), label: "Academics"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notifications"),
         ],
       ),
     );
