60a411bbf7cf1ecadfb01d0fe0a167c7f9effab4
Sarasi Yashodha <sarasiyashodha67@gmail.com>
2023-10-22 09:46:20 +0530

    Added logger

---
+++ lib/services/image_picker.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
+
+class FileImagePicker {
+  ImagePicker picker = ImagePicker();
+
+  Future<File?> pickImage() async{
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if(image != null) {
+      Logger().f(image.path);
+      return File(image.path);
+    }else{
+      Logger().e("Try again");
+      return null;
+    }
+  }
+}
